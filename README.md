af6fede01885b25de042a76750df78be09acfb43
Juan Gallo <juanfgallo78@gmail.com>
2023-07-22 18:23:27 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Api template para novos projetos
---
+++ src/main/java/br/com/arboretto/ApiArborettoApplication.java
@@ -0,0 +1,13 @@
+package br.com.arboretto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ApiArborettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiArborettoApplication.class, args);
+	}
+
+}
