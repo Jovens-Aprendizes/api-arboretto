39f9c47ea8ae5c41b2a54ac05c86f2574e049c95
Juan Gallo <juanfgallo78@gmail.com>
2023-08-26 16:17:15 -0300

    Subindo Rotas de Atualizar,Listar,Deletar e Login

--- src/main/java/br/com/arboretto/repository/UsuarioRepository.java
+++ src/main/java/br/com/arboretto/repository/UsuarioRepository.java
@@ -1,8 +1,21 @@
 package br.com.arboretto.repository;
 
+import java.util.List;
+
 import br.com.arboretto.model.Usuario;
 
 public interface UsuarioRepository {
+	
+	
 	public long salvar(Usuario usuario);
+	
 	public Usuario getPorId(String id);
+	
+	public int atualizar (Usuario usuario);
+	
+	public String getLogin (String cpf,String senha);
+	
+	public List<Usuario> listarUsuario();
+	
+	public void delete(String id);
 }
--- src/main/java/br/com/arboretto/service/UsuarioService.java
+++ src/main/java/br/com/arboretto/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package br.com.arboretto.service;
 
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import br.com.arboretto.model.Usuario;
 import br.com.arboretto.repository.jdbc.UsuarioRepositoryJdbc;
 
 import br.com.arboretto.validations.CPF;
+import br.com.arboretto.validations.Numero;
+
 
 @Service
 public class UsuarioService {
@@ -73,6 +77,107 @@ public class UsuarioService {
         }
        long l=usuarioRepositoryJdbc.salvar(usuario);
        return usuarioRepositoryJdbc.getPorId(String.valueOf(l));
+	}
+	
+	
+	public Usuario atualizar (Usuario usuario) {
+		
+		if (StringUtils.isBlank(usuario.getNome())) {
+            throw new RegraNegocioException("O Usuario deve ser informado");
+        }
+		
+		if (StringUtils.isBlank(usuario.getSenha())) {
+            throw new RegraNegocioException("A Senha deve ser informada");
+        }
+		
+		if (usuario.getNome().length() > 120) {
+            throw new RegraNegocioException("O Nome execede ao limite permitido.");
+        }
+		
+		if (usuario.getSenha().length() > 120) {
+            throw new RegraNegocioException("A Senha execede ao limite permitido.");
+        }
+		
+		if (StringUtils.isNotBlank(usuario.getCpf()) && (usuario.getCpf().length() == 11)
+                && !CPF.isCPF(usuario.getCpf())) {
+            throw new RegraNegocioException("O CPF é inválido.");
+        }
+		
+		if (StringUtils.isBlank(usuario.getEmail())) {
+            throw new RegraNegocioException("Email deve ser informado.");
+        }
+
+        if (usuario.getNome().length() > 60) {
+            throw new RegraNegocioException("Email excede 60 dígitos.");
+        }
+        
+        if (StringUtils.isBlank(usuario.getNumeroApartamento())) {
+            throw new RegraNegocioException("o Número do apartamento deve ser informado.");
+        }
+        
+        if (usuario.getNumeroApartamento().length()>3) {
+            throw new RegraNegocioException("o Número do apartamento não pode exceder o limite.");
+        }
+        
+        if (StringUtils.isBlank(usuario.getBloco())) {
+            throw new RegraNegocioException("o Número do bloco deve ser informado.");
+        }
+        
+        if (usuario.getBloco().length()>2) {
+            throw new RegraNegocioException("o Número do bloco deve conter 2 caracteres.");
+        }
+      
+        
+        if (StringUtils.isBlank(usuario.getDataNascimento())) {
+            throw new RegraNegocioException("A data de nascimento deve ser informada.");
+        }
+        
+        usuarioRepositoryJdbc.atualizar(usuario);
+        
+      return usuario;
+	
+		
+	}
+	
+	public Usuario getLogin(String cpf, String senha) {
+
+		if (StringUtils.isBlank(cpf) || StringUtils.isBlank(senha)) {
+			throw new RegraNegocioException("Cpf e senha devem ser informados.");
+		}
+
+		String id = usuarioRepositoryJdbc.getLogin(cpf, senha);
+
+		if (id == null) {
+			throw new RegraNegocioException("Cpf ou senha não conferem.");
+		}
+
+		return this.getPorId(id);
+
+	}
+	
+	public void delete(String id) {
+
+		if (StringUtils.isBlank(id)) {
+			throw new RegraNegocioException("O ID do Usuario deve ser informado.");
+		}
+
+		if (!Numero.isLong(id)) {
+			throw new RegraNegocioException("O ID do Usuario informado é inválido.");
+		}
+
+		usuarioRepositoryJdbc.delete(id);
+
+	}
+	
+	public Usuario getPorId(String id) {
+		
+		return usuarioRepositoryJdbc.getPorId(id);
+	}
+	
+	public  List<Usuario> listarUsuario(){
+		
+		return usuarioRepositoryJdbc.listarUsuario();
+		
 	}
 	
 }
